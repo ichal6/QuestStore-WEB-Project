18db9db6ffce4df8b659852261c48ca4f255b26c
Rafal <rpajdak@gmail.com>
2020-05-23 17:04:24 +0200

    Add validation for my account page

--- js/input-validator.js
+++ js/input-validator.js
@@ -3,14 +3,21 @@ const passw_input = document.querySelector(".passw-input");
 const warning_paragraph = document.querySelector(".warning");
 let second_passw_input = document.querySelector(".sec-passw");
 let resetPasswBtn = document.querySelector(".button");
-
+let name_input = document.querySelector(".name-surname");
 const submitBtn = document.querySelector(".submit-button");
+
 
 function emailValidator() {
     var mailformat = /([a-zA-z_\-.\]+)@([a-zA-z0-9-]+)\.([a-z\.])/
     let email_value = email_input.value;
     let validEmail = mailformat.test(email_value);
-    displayWarning(validEmail);
+    const email_paragraph = document.querySelector(".warning-email");
+    console.log(validEmail)
+    if(!validEmail){
+        email_paragraph.classList.remove("hidden");
+    }else{
+        email_paragraph.classList.add("hidden");
+    }
 }
 function passwordValidator() {
     var passwFormat = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
@@ -30,12 +37,26 @@ function checkMatchPasswords(){
     }
 }
 
+function nameValidator(){
+    const regExForName = /^([a-zA-Z]{2,}\s[a-zA-z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/;
+    let name_value = name_input.value;
+    let validName = regExForName.test(name_value);
+    displayWarning(validName);
+}
+
+
 function displayWarning(validatorResult){
     if (validatorResult == false) {
         warning_paragraph.classList.remove("hidden");
     } else {
         warning_paragraph.classList.add("hidden");
     }
+}
+function checkEmail(){
+    email_input.addEventListener("focusout",emailValidator);
+}
+function checkName(){
+    name_input.addEventListener("focusout",nameValidator);
 }
 
 function checkIfPasswordsAreEquals(){
