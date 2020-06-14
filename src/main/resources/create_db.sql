a37f7ce8755e5fa6f461e354fdc0b05b1ec6c0f8
Rafal Pajdak <rpajdak@gmail.com>
2020-06-14 12:57:08 +0200

    Remove insert part of file

--- src/main/resources/create_db.sql
+++ src/main/resources/create_db.sql
@@ -63,7 +63,7 @@ CREATE TABLE codecooler(
 	picture_url varchar(100),
 	class_id integer,
 	team_id integer,
-	wallet_id integer NOT NULL,
+	wallet_id serial NOT NULL,
 	CONSTRAINT krzystof FOREIGN KEY (class_id)
 	REFERENCES class (class_id)
 	ON UPDATE CASCADE,
@@ -135,98 +135,4 @@ CREATE TABLE team_artifacts(
 	CONSTRAINT zbyszek FOREIGN KEY(artifact_id)
  	REFERENCES artifact(artifact_id)
 	ON UPDATE CASCADE
-);	
-
-INSERT INTO cms_user
-VALUES (default,'Jan Truskolaski','jtruskolaski@gmail.com','admin123','Krakow',TO_DATE('2020-01-01','YYYY-MM-DD'),'URL','Y');
-
-INSERT INTO cms_user
-VALUES (default,'Andrzej Szampan','aszampan@gmail.com','admin123','Krakow',TO_DATE('2020-01-01','YYYY-MM-DD'),'URL','Y');
-
-INSERT INTO cms_user
-VALUES (default,'Dominik Nowak','dnowak@gmail.com','admin123','Krakow',TO_DATE('2020-01-01','YYYY-MM-DD'),'URL','N');
-
-INSERT INTO cms_user
-VALUES (default,'Paweł Suryk','psuryk@gmail.com','admin123','Krakow',TO_DATE('2020-01-01','YYYY-MM-DD'),'URL','N');
-
-
-INSERT INTO class 
-values (default,'Class #1','Krakow',TO_DATE('2020-06-01','YYYY-MM-DD'),TO_DATE('2021-06-01','YYYY-MM-DD'));
-
-INSERT INTO team
-values(default,'Team #1 in Krakow','Krakow',TO_DATE('2020-07-07','YYYY-MM-DD'));
-
-INSERT INTO wallet
-values(default);
-
-INSERT INTO codecooler
-values(default,'Paweł Byczek',
-'pbyczek@gmail.com',
-'pbyczek123',
-'Krakow',
-TO_DATE('2020-06-01','YYYY-MM-DD'),
-'mojezdjecie',
-1,
-1,
-1);
-
-
-INSERT INTO level
-values(default,'Level 1 - Iron Maiden','Basic level on yuur journey','200',default,'level1.png');
-
-INSERT INTO level
-values(default,'Level 2 - Silver Warrior','You are getting stronger..','500',default,'level2.png');
-
-INSERT INTO level
-values(default,'Level 3 - Golden Level','Higher, higher.','1000',default,'level3.png');
-
-INSERT INTO level
-values(default,'Level 4 - Platinium Warrior','Halfway of the journey.','2500',default,'level4.png');
-
-INSERT INTO level
-values(default,'Level 5 - Stealth Ninja','Almost on the top.','5000',default,'level5.png');
-
-INSERT INTO level
-values(default,'Level 6 - Codecool Buddha','There is nothing more.','10000',default,'level6.png');
-
-INSERT INTO quest
-values(default,'Passing a checkpoint.','You have passed a checkopint.','500','0',default,'quest1.png');
-
-INSERT INTO quest
-values(default,'Spot a mistake in the assigment.','You fund a mistake in background materials.','500','1',default,'quest2.png');
-
-INSERT INTO quest
-values(default,'Do a demo for the class.','You did a demo for the class.','1000','1',default,'quest3.png');
-
-INSERT INTO quest
-values(default,'Take a part in student screening.','Blablabla','5000','1',default,'quest4.png');
-
-INSERT INTO quest
-values(default,'Attend a month without being late.','Always on time!','10000','1',default,'quest5.png');
-	
-INSERT INTO quest
-values(default,'Set a smart Goal with your tutorial.','Blablabla','10000','1',default,'quest6.png');
-
-INSERT INTO artifact
-values(default,'Private mentoring.','Blablabla','500','0',default,'quest1.png');
-
-INSERT INTO artifact
-values(default,'Spend a day on home office.','Blablabla','500','0',default,'quest1.png');
-
-INSERT INTO artifact
-values(default,'Extend an SI deadline for a week. ','Blablabla','1000','0',default,'quest1.png');
-
-INSERT INTO artifact
-values(default,'60 minutes workshop by mentor.','Blablabla','5000','1',default,'quest1.png');
-
-INSERT INTO artifact
-values(default,'Mentor joins student for 1 hour.','Blablabla','10000','1',default,'quest1.png');
-
-INSERT INTO artifact
-values(default,'Extra materials for chosen topic','Blablabla','10000','1',default,'quest1.png');
-
-
-
-
-
-
+);
