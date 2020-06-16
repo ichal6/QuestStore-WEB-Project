9e37594163b1737db30b67d144d6a1e26de547bb
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-06-16 15:45:12 +0200

    Attach database for CMSUserLogin

--- src/main/java/Controller/CMSUserLogin.java
+++ src/main/java/Controller/CMSUserLogin.java
@@ -1,5 +1,8 @@
 package Controller;
 
+import DAO.IUserDAO;
+import DAO.ReadException;
+import DAO.UserDBDAO;
 import Model.CMSUser;
 
 import javax.servlet.RequestDispatcher;
@@ -13,12 +16,7 @@ import java.util.Calendar;
 
 @WebServlet(name = "CMSUserLogin", urlPatterns = "/CMSUserLogin")
 public class CMSUserLogin extends HttpServlet {
-    private CMSUser userAdmin = new CMSUser(1,"NowyUser", "relaks@wp.pl", "1234",
-            "Kraków", new java.sql.Date(Calendar.getInstance().getTime().getTime()),
-            "simpleURL", true);
-    private CMSUser userMentor = new CMSUser(1,"Nowy Mentor", "mentor@wp.pl", "1234",
-            "Kraków", new java.sql.Date(Calendar.getInstance().getTime().getTime()),
-            "simpleURL", false);
+    private IUserDAO dao;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -29,19 +27,25 @@ public class CMSUserLogin extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        dao = new UserDBDAO("src/main/resources/database.properties");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-        boolean isCorrectLogIn = tryLogIn(email, password);
+        boolean isCorrectLogIn = false;
+        try {
+            isCorrectLogIn = dao.checkUser(email, password);
+        } catch (ReadException e) {
+            throw new ServletException(e);
+        }
         if(isCorrectLogIn){
-            CMSUser user = getUserFromDatabase(email, password);
+            CMSUser user = null;
+            try {
+                user = dao.getCMSUser(email, password);
+            } catch (ReadException e) {
+                throw new ServletException(e);
+            }
             SessionManager.setActualUser(user);
-            if(SessionManager.getActualUser().isAdmin()){
-                response.sendRedirect("/dashboard");
-            }
-            else{
-                response.sendRedirect("/dashboard");
-            }
+            response.sendRedirect("/dashboard");
         }
         else{
             String message = "<p class=\"warning-incorrect-login\">You put incorrect data!<p>";
@@ -49,24 +53,6 @@ public class CMSUserLogin extends HttpServlet {
             RequestDispatcher dispatcher
                     = request.getRequestDispatcher("/html-login-and-account/login.jsp");
             dispatcher.forward(request, response);
-        }
-    }
-
-    private boolean tryLogIn(String email, String password){
-        // It will be request to database
-        if(email.equals(userAdmin.getEmail()) && password.equals(userAdmin.getPassword())){
-            return true;
-        }
-        else return email.equals(userMentor.getEmail()) && password.equals(userMentor.getPassword());
-    }
-
-    private CMSUser getUserFromDatabase(String email, String password){
-        //here should be run DAO and read current user after positive result of tryLogIn method
-        if(email.equals("relaks@wp.pl")){
-            return userAdmin;
-        }
-        else{
-            return userMentor;
         }
     }
 }
