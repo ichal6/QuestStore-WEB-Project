295427f6799eb0018efaefaee8766a0ba47ddbec
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-06-17 18:34:11 +0200

    Create skeleton of classes

---
+++ src/main/java/Controller/AdminListController.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "AdminList", urlPatterns = "/admins")
+public class AdminListController extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("/html-cms/admins_list.jsp");
+    }
+}
