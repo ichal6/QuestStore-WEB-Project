a8588bbc16101f1ee4a0c64c9b9466dcbe7ee784
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-06-18 17:05:53 +0200

    Add common delete option

--- src/main/java/Controller/users/UserDeleteController.java
+++ src/main/java/Controller/users/UserDeleteController.java
@@ -1,8 +1,7 @@
-package Controller.admins;
+package Controller.users;
 
 import DAO.UserDAO;
 import DAO.UserJDBCDAO;
-import Model.CMSUser;
 import Exception.ReadException;
 
 import javax.servlet.ServletException;
@@ -13,10 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 
-@WebServlet(name = "AdminDeleteController", urlPatterns = "/admins/delete")
-public class AdminDeleteController extends HttpServlet {
+@WebServlet(name = "UserDeleteController", urlPatterns = "/user/delete")
+public class UserDeleteController extends HttpServlet {
     private UserDAO dao;
-    private CMSUser userToDelete;
 
     @Override
     public void init() throws ServletException {
@@ -32,24 +30,25 @@ public class AdminDeleteController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doDelete(request, response);
-        response.sendRedirect("/admins");
     }
 
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Map<String, String[]> parameters = request.getParameterMap();
         String id = parameters.get("id")[0];
+        String type = parameters.get("type")[0];
         int idUser = 0;
         try {
             idUser = Integer.parseInt(id);
         } catch(NumberFormatException ex){
             //throw new ServletException("You put incorrect path to page!");
-            response.sendRedirect("/admins");
+            response.sendRedirect("/dashboard");
         }
         try {
             dao.deleteUser(idUser);
         } catch (ReadException ex) {
             throw new ServletException("You cannot delete this user");
         }
+        response.sendRedirect("/user-list?type=" + type);
     }
 }
