6bf93805296c92770dd296b56ac648068fd50579
karbudzik <karolinabudzik92@gmail.com>
2020-07-14 17:25:51 +0200

    Add one test to QuestService

---
+++ src/test/java/service/QuestServiceTest.java
@@ -0,0 +1,65 @@
+package service;
+
+import DAO.ArtifactDAO;
+import DAO.QuestDAO;
+import exception.ReadException;
+import model.Artifact;
+import model.Quest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+class QuestServiceTest {
+
+    @InjectMocks
+    QuestService questService;
+
+    @Mock
+    QuestDAO questDAO;
+
+    @BeforeEach
+    public void init() throws ReadException {
+        MockitoAnnotations.initMocks(this);
+        given(questDAO.getAllQuests()).willReturn(prepareMockData());
+    }
+
+    @Test
+    public void shouldGetUnsortedQuestsIfSortingParametersDontExist() throws ReadException {
+        // given:
+        String sortBy = null;
+        Boolean order = null;
+        List<Quest> questListBeforeSort = prepareMockData();
+
+        // when:
+        List<Quest> questListFromService = questService.getAllQuests(sortBy, order);
+
+        // then:
+        Assertions.assertEquals(questListBeforeSort.get(0).getID(), questListFromService.get(0).getID());
+        Assertions.assertEquals(questListBeforeSort.get(1).getID(), questListFromService.get(1).getID());
+    }
+
+    private List<Quest> prepareMockData() {
+        Quest quest1 = new Quest();
+        quest1.setID(11);
+        Quest quest2 = new Quest();
+        quest2.setID(12);
+        Quest quest3 = new Quest();
+        quest3.setID(13);
+        List<Quest> questList = new ArrayList<>();
+        questList.add(quest1);
+        questList.add(quest2);
+        questList.add(quest3);
+
+        return questList;
+    }
+}
