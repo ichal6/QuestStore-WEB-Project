28d1915524bfffbac7a3665081a5625f8c427921
karbudzik <karolinabudzik92@gmail.com>
2020-07-14 12:25:26 +0200

    Add mockito dependencies

---
+++ src/test/java/service/ArtifactServiceTest.java
@@ -0,0 +1,19 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ArtifactServiceTest {
+
+    @Mock
+
+
+    @Test
+    public void callArtifactUsageChange() {
+    }
+}
