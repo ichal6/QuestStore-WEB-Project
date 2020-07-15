a9689973e0f3337e60b537f4beef0891f82897a8
karbudzik <karolinabudzik92@gmail.com>
2020-07-15 13:41:07 +0200

    Finish and correct QuestServiceTest methods

--- src/test/java/service/QuestServiceTest.java
+++ src/test/java/service/QuestServiceTest.java
@@ -12,7 +12,10 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class QuestServiceTest {
 
@@ -29,7 +32,7 @@ class QuestServiceTest {
     }
 
     @Test
-    public void shouldGetUnsortedQuestsIfSortingParametersDontExist() throws ReadException {
+    public void should_get_unsorted_quests_if_sorting_parameters_dont_exist() throws ReadException {
         // given:
         String sortBy = null;
         Boolean order = null;
@@ -42,16 +45,59 @@ class QuestServiceTest {
         Assertions.assertEquals(questListBeforeSort.get(0).getID(), questListFromService.get(0).getID());
         Assertions.assertEquals(questListBeforeSort.get(1).getID(), questListFromService.get(1).getID());
         Assertions.assertEquals(questListBeforeSort.get(2).getID(), questListFromService.get(2).getID());
+        verify(questDAO, times(1)).getAllQuests();
+    }
+
+    @Test
+    public void should_sort_quests_by_name_ascending() throws ReadException {
+        // given:
+        String sortBy = "name";
+        Boolean order = true;
+
+        // when:
+        List<Quest> sortedQuestsList = questService.getAllQuests(sortBy, order);
+
+        // then:
+        Assertions.assertEquals(sortedQuestsList.get(0).getName(), "A");
+        Assertions.assertEquals(sortedQuestsList.get(1).getName(), "B");
+        Assertions.assertEquals(sortedQuestsList.get(2).getName(), "G");
+        verify(questDAO, times(1)).getAllQuests();
+    }
+
+    @Test
+    public void should_sort_quests_by_value_descending() throws ReadException {
+        // given:
+        String sortBy = "value";
+        Boolean order = false;
+
+        // when:
+        List<Quest> sortedQuestsList = questService.getAllQuests(sortBy, order);
+
+        // then:
+        Assertions.assertEquals(sortedQuestsList.get(0).getValue(), 100);
+        Assertions.assertEquals(sortedQuestsList.get(1).getValue(), 10);
+        Assertions.assertEquals(sortedQuestsList.get(2).getValue(), 1);
+        verify(questDAO, times(1)).getAllQuests();
     }
 
     private List<Quest> prepareMockData() {
         Quest quest1 = new Quest();
         quest1.setID(11);
+        quest1.setName("A");
+        quest1.setValue(1);
+
         Quest quest2 = new Quest();
         quest2.setID(8);
+        quest2.setName("G");
+        quest2.setValue(10);
+
         Quest quest3 = new Quest();
         quest3.setID(22);
+        quest3.setName("B");
+        quest3.setValue(100);
+
         List<Quest> questList = new ArrayList<>();
+
         questList.add(quest1);
         questList.add(quest2);
         questList.add(quest3);
