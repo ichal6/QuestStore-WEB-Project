4c56daa9ac10d0772807c2ab1859cafa97e07afd
michal <kuk.michal3@gmail.com>
2020-07-18 12:15:18 +0200

    add method for exception testing

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -46,6 +46,19 @@ class UserServiceTest {
         verify(dao, times(1)).getAllAdmins();
     }
 
+    @Test
+    void should_throw_read_exception() throws ReadException {
+        //given:
+        String userType = "admin";
+        String sortBy = "name";
+        Boolean order = true;
+        //when:
+        given(dao.getAllAdmins()).willThrow(new ReadException(" "));
+        //then:
+        Assertions.assertThrows(ReadException.class, ()->userService.getAllUsers(userType, sortBy, order));
+
+    }
+
     private List<CMSUser> prepareMockData() {
         CMSUser user1 = new CMSUser.Builder()
                 .userName("Michal")
